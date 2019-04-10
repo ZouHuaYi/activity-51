103210d4404b52184b4eb703c260a3f0169a7cd6
zouhuayi <1004753639@qq.com>
2019-04-10 23:01:22 +0800

    x0

--- src/api/login.js
+++ src/api/login.js
@@ -13,12 +13,32 @@ export async function loginApi(params) {
     })
 }
 
-export async function sendCOde(phone) {
+export async function sendCOde(params) {
     return request('/rest/user/send_code',{
         method:'POST',
         body:{
-            phone: phone,
+            phone: params.phone,
             type:5,
         }
     })
+}
+
+export async function bindWxchatApi(params) {
+  return request('/rest/user/open_bind_unbundled',{
+    method:'POST',
+    body:{
+      wechat: params.unionId,
+      unionId:params.unionId,
+      id:params.id,
+      action:0,
+    }
+  })
+}
+
+export async function bindParent(params) {
+
+}
+
+export async function saveAvter(params) {
+
 }
--- src/utils/jscookie.js
+++ src/utils/jscookie.js
@@ -2,7 +2,7 @@
  * Created by zhy on 2019/3/19.
  */
 const MSM_TOKEN = 'Admin-Token';
-
+const MSM_USERID = 'User-Id';
 
 export function setToken(value) {
   return window.sessionStorage.setItem(MSM_TOKEN,value);
@@ -10,4 +10,12 @@ export function setToken(value) {
 
 export function getToken() {
   return window.sessionStorage.getItem(MSM_TOKEN);
+}
+
+export function setUserId(value) {
+  return window.sessionStorage.setItem(MSM_USERID,value);
+}
+
+export function getUserId() {
+  return window.sessionStorage.getItem(MSM_USERID);
 }
